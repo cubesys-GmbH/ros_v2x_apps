bd6161db92138b5c428316b87e6f3ee7a614c6c4
Apostolos Georgiadis <apostolos.georgiadis@nfiniity.com>
2024-10-04 16:11:34 +0200

    cpm_provider: formatting

--- dev_ws/src/v2x_apps/v2x_apps/cpm_provider.py
+++ dev_ws/src/v2x_apps/v2x_apps/cpm_provider.py
@@ -6,18 +6,21 @@ from typing import Optional
 from rclpy.node import Node
 from vanetza_msgs.msg import PositionVector
 
+
 class CpmProvider(Node):
     def __init__(self):
         super().__init__('cpm_provider')
         self.get_logger().info(f'Node "{self.get_name()}" started')
-        
+
         # We are interested in our own position so we can set a reasonable destination area
         self.position_vector: Optional[PositionVector] = None
-        self.pos_vector_subscription = self.create_subscription(PositionVector, '/its/position_vector', self.position_update, 1)
-        
+        self.pos_vector_subscription = self.create_subscription(
+            PositionVector, '/its/position_vector', self.position_update, 1)
+
         # Publish who provides CPM to cube-its
-        self.cpm_publisher = self.create_publisher(cpm_msg.CollectivePerceptionMessage, '/its/cpm_provided', 1)
-        
+        self.cpm_publisher = self.create_publisher(
+            cpm_msg.CollectivePerceptionMessage, '/its/cpm_provided', 1)
+
         # Here we just provide a CPM to cube-its every 1 seconds.
         self.create_timer(timer_period_sec=1.0, callback=self.publish)
 
@@ -29,7 +32,7 @@ class CpmProvider(Node):
         """Reference position is at our own position for this example."""
         if self.position_vector is None:
             raise RuntimeError('No position vector available')
-        
+
         # Reference position
         pos = cpm_msg.ReferencePosition()
         pos.latitude.value = (
@@ -74,7 +77,7 @@ class CpmProvider(Node):
         )
         pos.altitude.altitude_confidence.value = cpm_msg.AltitudeConfidence.UNAVAILABLE
         return pos
-    
+
     def generate_perceived_object_cpm(self) -> cpm_msg.CollectivePerceptionMessage:
         """Generate a CPM with use-case specific data."""
         # Perceived object
@@ -95,25 +98,27 @@ class CpmProvider(Node):
         perceived_object.object_dimension_x_is_present = True
         perceived_object.object_dimension_x.value.value = 30
         perceived_object.object_dimension_x.confidence.value = 1
-        
+
         # Container
         container = cpm_msg.WrappedCpmContainer()
         container.container_id.value = 5
         container.container_data.choice.value = 5
         container.container_data.perceived_object_container.number_of_perceived_objects.value = 1
-        container.container_data.perceived_object_container.perceived_objects.array = [perceived_object]
-        
+        container.container_data.perceived_object_container.perceived_objects.array = [
+            perceived_object]
+
         cpm = cpm_msg.CollectivePerceptionMessage()
         cpm.payload.management_container.reference_position = self.get_reference_position()
         cpm.payload.cpm_containers.value.array = [container]
         return cpm
-        
+
     def publish(self) -> None:
         """Publish on topic"""
         if self.position_vector:
             cpm = self.generate_perceived_object_cpm()
             self.cpm_publisher.publish(cpm)
             self.get_logger().info(f'CPM provided')
+
 
 def main(args=None):
     rclpy.init(args=args)
